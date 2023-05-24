9cb9365b1a1c698e189d9112d7c80e668a9879bc
Christopher <qaetre@gmail.com>
2023-05-24 17:04:06 -0400

    complete lab with stretch goal of delete button

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
 });
+const dumb = document.getElementById("create-task-form")
+dumb.addEventListener("submit", (e) => {
+  e.preventDefault()
+  let newTask = document.createElement("li")
+  let newButton = document.createElement("button")
+  newTask.textContent = document.getElementById('new-task-description').value
+  newTask.dataset.description = document.getElementById('new-task-description').value
+  newButton.dataset.description = document.getElementById('new-task-description').value
+  newButton.addEventListener("click", e => {
+    const deleteMe = e.target.dataset.description
+    const taskManager = document.getElementById('tasks')
+    taskManager.removeChild(document.querySelector(`[data-description="${deleteMe}"]`))
+  })
+  let taskList = document.getElementById('tasks')
+  newTask.append(newButton)
+  taskList.appendChild(newTask)
+  e.target.reset()
+  
+})
